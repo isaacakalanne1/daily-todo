f3ea605d5ffa9807376159b2129082df5d40d6e8
Isaac Akalanne <iakalann@jaguarlandrover.com>
2023-02-22 20:54:51 +0000

    Initial app commit

---
+++ Daily Todo/Daily Todo/Daily_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_TodoApp.swift
+//  Daily Todo
+//
+//  Created by iakalann on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Daily_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
